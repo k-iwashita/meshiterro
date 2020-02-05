0d4498cdeeead8d54b5ceca36b60d95d8cdba8c6
k-iwashita <k.iwashita.gude@gmail.com>
2020-02-05 11:52:39 +0900

    雛形の完成

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'cd4c7549bec5621157bb2b2a2da57361af5f9af462c2237825565a29f837eef3abb1a77e5a7d2faee8d48d0b29d3da9d6c2037b00942dae41e33ca264f835674'
